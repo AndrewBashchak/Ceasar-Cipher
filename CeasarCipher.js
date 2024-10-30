40f7a8a59b7d6dff5c15ca646b24683e8ea6ff81
AndrewBashchak <g.a.p.bloger@gmail.com>
2024-10-30 21:01:57 +0200

    CeasarCipher

---
+++ CeasarCipher.js
@@ -0,0 +1,24 @@
+function caesarCipher(str, shift) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        if (char >= "A" && char <= "Z") {
+            result += String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65);
+        }
+        
+        else if (char >= "a" && char <= "z") {
+            result += String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97);
+        } else {
+            result += char;
+        }
+    }
+
+    return result;
+}
+
+let text = "UkD";
+let shift = -2;
+let encryptedText = caesarCipher(text, shift);
+console.log(encryptedText)
